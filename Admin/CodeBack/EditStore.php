a9244656f3db7c28a9be45ac99dd38cd6e88414b
BirdFIre <kokoit555@gmail.com>
2017-12-11 16:59:56 +0700

    แก้นิดหน่อยส่วน Update

--- Admin/CodeBack/EditStore.php
+++ Admin/CodeBack/EditStore.php
@@ -11,6 +11,36 @@ if(!empty($_GET['idstore'])){
 }
 
 if(!empty($_POST['submiteditstore'])){
+    $namestore = mysqli_escape_string($connect,$_POST['namestore']); 
+    $telephone = mysqli_escape_string($connect,$_POST['telephone']); 
+    $address = mysqli_escape_string($connect,$_POST['address']); 
+    $city = mysqli_escape_string($connect,$_POST['city']); 
+    $state = mysqli_escape_string($connect,$_POST['state']); 
+    $country = mysqli_escape_string($connect,$_POST['country']); 
+    $zip = mysqli_escape_string($connect,$_POST['zip']); 
+    $nameAccountStore = mysqli_escape_string($connect,$_POST['nameAccountStore']); 
+    $numberStorebank = mysqli_escape_string($connect,$_POST['numberStorebank']); 
+    $namebank = mysqli_escape_string($connect,$_POST['namebank']); 
+    $storytextstore = mysqli_escape_string($connect,$_POST['storytextstore']);
+    // $email = mysqli_escape_string($connect,$_POST['email']); 
+    // $password = MD5(mysqli_escape_string($connect,$_POST['password'])); 
+
+    $sqlupdate = "UPDATE `store` SET 
+    `NameStore` = '',
+    `AvatarStore` = '',
+    `AddressStore` = '',
+    `TelStore` = '',
+    `CityStore` = '',
+    `StateStore` = '',
+    `ZipStore` = '',
+    `CountryStore` = '',
+    `EmailStore` = '',
+    `Password` = '',
+    `textStory` = '',
+    `nameAccountStore` = '',
+    `numberStorebank` = '',
+    `namebank` = '' 
+    WHERE `id_store` = '".$_GET['idstore']."'";
 
 }
 ?>
--- Admin/tableOrderStore.php
+++ Admin/tableOrderStore.php
@@ -15,11 +15,11 @@
                         <?php
                             include "../Codephp/connectdb.php";
                             $idstore = $_GET['idstore'];
-                            $select = "SELECT * , sps.Status statusshipment FROM `Order_product` op
-                                        INNER JOIN User_member um ON um.id_user = op.id_user
-                                        INNER JOIN Store_product_shipment sps ON sps.id_order = op.id_order
-                                        INNER JOIN Shipping s ON s.id_Shipping = sps.Id_shipping
-                                        INNER JOIN orderproductdetail opd ON opd.id_order = op.id_order
+                            $select = "SELECT DISTINCT op.id_order,um.Name,s.NameShipping , op.Date_order , p.NameProduct , op.Totalprice , sps.Status FROM `Order_product` op 
+                                        INNER JOIN User_member um ON um.id_user = op.id_user 
+                                        INNER JOIN Store_product_shipment sps ON sps.id_order = op.id_order 
+                                        INNER JOIN Shipping s ON s.id_Shipping = sps.Id_shipping 
+                                        INNER JOIN orderproductdetail opd ON opd.id_order = op.id_order 
                                         INNER JOIN product p ON p.id_product = opd.id_product
                                         WHERE p.id_store = '$idstore'
                                         ORDER BY opd.id_orderDetail DESC";
@@ -36,33 +36,37 @@
                                     <td><?php echo $row['Date_order']; ?></td>
                                     <td><?php echo $row['NameProduct']; ?></td>
                                     <td><?php echo number_format($row['Totalprice']); ?></td>
-                                    <td><?php if($row['statusshipment'] == '1'){echo "Success";}
-                                              else if($row['statusshipment'] == '0'){echo "Pedding";} ?></td>
-                                    <td><button data-toggle="modal" data-target="#infoOrder" class="btn btn-info">ดูข้อมูลเชิงลึก</button></td>
+                                    <td><?php if($row['Status'] == '1'){echo "Success";}
+                                              else if($row['Status'] == '0'){echo "Pedding";} ?></td>
+                                    <td><a href="#infoOrder<?php echo $row['id_order']; ?>" data-toggle="modal"  class="btn btn-info">ดูข้อมูลเชิงลึก</a></td>
                                 </tr>
+
+                                <div class="modal fade" id="infoOrder<?php echo $row['id_order']; ?>" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                <h4 class="modal-title" id="myModalLabel">Large Modal</h4>
+                                            </div>
+                                            <div class="modal-body">
+
+                                                <h3><?php echo $row['id_order']; ?></h3>
+
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>  
                                 <?php
                                 }
                             }
-            
+                            
                             mysqli_close($connect);
                         ?>
                         </tbody>
                     </table>
 
-                    <div class="modal fade" id="infoOrder" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
-                        <div class="modal-dialog modal-lg">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                    <h4 class="modal-title" id="myModalLabel">Large Modal</h4>
-                                </div>
-                                <div class="modal-body">
-                                    <h3>Modal Body</h3>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
